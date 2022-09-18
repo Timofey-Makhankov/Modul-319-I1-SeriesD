0f04d8b827c4bf73bdf7ec890e98da734c43a95c
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-18 20:18:31 +0200

    added the ability to work with floating point numbers

--- src/ch/tbz/Main.java
+++ src/ch/tbz/Main.java
@@ -14,27 +14,40 @@ public class Main {
     // Our main function which runs the program12
     public static void main(String[] args ) {
         Random rand = new Random();
-        int result = 0;
+        double result = 0;
         boolean is_running = true;
         while (is_running) {
+
             char user_input = inputChar("What do you want to train? +, - or * ?");
-            int randomInt1 = rand.nextInt(101);
-            int randomInt2 = rand.nextInt(101);
+            float randomInt1 = rand.nextInt(101);
+            float randomInt2 = rand.nextInt(101);
+
             if (user_input == '+') {
+                randomInt1 += roundAvoid(random(), 1);
+                randomInt2 += roundAvoid(random(), 1);
                 result = Addition(randomInt1, randomInt2);
             } else if (user_input == '-') {
+                randomInt1 += roundAvoid(random(), 1);
+                randomInt2 += roundAvoid(random(), 1);
                 result = Subtraction(randomInt1, randomInt2);
             } else{
                 result = Multiplication(randomInt1, randomInt2);
             }
+            result = roundAvoid(result, 1);
+
             System.out.println(randomInt1 + " " + user_input + " " + randomInt2 + " = ?");
-            int answer = inputInt("Enter the result:");
+
+            double answer = inputDouble("Enter the result:");
+            answer = roundAvoid(answer, 1);
+
             if (answer != result) {
                 out.println("WRONG: The Result is " + result);
             } else {
                 out.println("CORRECT, The Result is " +  result);
             }
+
             String decision = inputString("Do you want to test again? Yes or no?").toLowerCase();
+            
             if (decision.equals("no")) {
                 out.println("Okay, goodbye");
                 is_running = false;
@@ -42,13 +55,19 @@ public class Main {
 
         }
     }
-    static int Addition(int num1, int num2){
+    static float Addition(float num1, float num2){
         return num1 + num2;
     }
-    static int Subtraction(int num1, int num2){
+    static float Subtraction(float num1, float num2){
         return num1 - num2;
     }
-    static int Multiplication(int num1, int num2){
+    static float Multiplication(float num1, float num2){
         return num1 * num2;
+    }
+
+    //! Source = https://www.baeldung.com/java-round-decimal-number
+    public static double roundAvoid(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 }
